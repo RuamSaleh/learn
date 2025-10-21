b1c28d123dd83b2032e27ddc644c4c93df7daf79
ruam <roamsaleh@gmail.com>
2025-10-22 00:12:35 +0300

    Initial Commit

---
+++ learnInAweek/learnInAweekApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnInAweekApp.swift
+//  learnInAweek
+//
+//  Created by ruam on 30/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct learnInAweekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learnInAweekTests/learnInAweekTests.swift
@@ -0,0 +1,17 @@
+//
+//  learnInAweekTests.swift
+//  learnInAweekTests
+//
+//  Created by ruam on 30/04/1447 AH.
+//
+
+import Testing
+@testable import learnInAweek
+
+struct learnInAweekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
